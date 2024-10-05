89b480db81f9eed81afc94824bb611459c53fa05
kmayank632 <84236141+kmayank632@users.noreply.github.com>
2024-10-05 17:44:20 +0530

    Create randomMessages.ts

---
+++ src/utils/randomMessages.ts
@@ -0,0 +1,9 @@
+const randomMessages = [
+    "Hello from the server!",
+    "How's your day going?",
+    "Random message for you!",
+    "Hope you're doing great!",
+    "This is a random response."
+];
+
+export default randomMessages
